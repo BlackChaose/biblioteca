54545d148d04561189b0421bfcc102248b3186e1
Nikita <nikita.s.kalitin@gmail.com>
2020-07-02 00:19:21 +0300

    fix(bug)

--- src/Controllers/BibliotecaController.php
+++ src/Controllers/BibliotecaController.php
@@ -52,9 +52,9 @@ class BibliotecaController extends Controller
         try {
             $data = $request->validate([
                 'name' => 'string|min:3|max:255|required',
-                'description' => 'string|min:3|max:255|required',
-                'lang' => 'string|min:3|max:255|required',
-                'author' => 'string|min:3|max:255|required',
+                'description' => 'string|min:3|max:255',
+                'lang' => 'string|min:3|max:255',
+                'author' => 'string|min:3|max:255',
             ]);
 
             if (empty($data)) {
